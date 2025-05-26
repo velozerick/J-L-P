734ef55503b6ea0f42adf0ae35719797a7c69092
Erick Veloz <158847938+velozerick@users.noreply.github.com>
2025-05-25 21:12:00 -0600

    Update Hello_There.java

--- 1_CH/Primer_Programa/Hello_There.java
+++ 1_CH/Primer_Programa/Hello_There.java
@@ -6,6 +6,40 @@ public class F_S_P {
 		
 		System.out.println("This is a simple Java program");
 
+		System.out.println("This is a simple Java program");
+		System.out.println("Hola mami te quiero");
+		
+		//EJERCICIOS
+		// 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
+		
+		System.out.println("Mi nombre es Erick");
+
+        // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
+		System.out.println("Hola\nMundo");
+
+        // 3. Añade un comentario sobre lo que hace cada línea del programa.
+		System.out.println("Hello there"); //Imprime en pantalla
+
+        // 4. Crea un comentario en varias líneas.
+		/*
+		 * Si esto 
+		 * es
+		 * un comentario en varias
+		 * lineas
+		 */
+
+        // 5. Imprime tu edad, tu color favorito y tu ciudad.
+		System.out.println("Mi nombre es Erick\nMi color favorito es el cuatro\ny\nMi ciudad es Mexico.");
+
+        // 6. Explora los diferentes System.XXX.println(); más allá de "out".
+		System.err.println("que es?");
+		System.out.println("F_S_P.main()");
+		
+        // 7. Utiliza varios println para imprimir una frase.
+		System.out.println("Esto");
+		System.out.println("Es una");
+		System.out.println("frase");
+
 	}
 
 }
