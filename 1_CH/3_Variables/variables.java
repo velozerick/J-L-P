f2283a7268a3bef43b21aacd9d029fe368da4154
Erick Veloz <158847938+velozerick@users.noreply.github.com>
2025-05-25 20:53:39 -0600

    Update variables.java

--- 1_CH/3_Variables/variables.java
+++ 1_CH/3_Variables/variables.java
@@ -3,5 +3,34 @@ VARIABLES
 
 las variables es la unidad basica de almacenamiento en java , es definida por la combinacion de un identiifcador , un tipo y un inicializador es opcional 
 
+Los identiifcadores o nombre de la variable deben cumplir ciertas reglas :
+
+- Deben empezar por una letra y en general, no contener simbolos especiales excepto el subrayado _
+-No deben coincidir con alguna palabra reservada del lenguaje, tales como class o function
+
+-------------------------------------------
+VALIDO                     NO VALIDO
+___________________________________________
+x                           5x
+x5                          _pepe
+pepe
+pepe_luis                   pepe!luis
+                            raiz
+-------------------------------------------
+
+
+
+es imprescindible indicar cual va a ser el identiifcador de la variable y que tipo de dato va almacenar a eso se le llama declarar variable
+
+//Declaramos de que tipo son nuestras variables
+
+int x;
+double y;
+char z;
+
+//Declaramos variables
+x = 5;
+y = 7.445;
+z = 'hello';
 
 */
