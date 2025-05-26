3154b9295a28954698c57b3bef7897e3909282a0
Erick Veloz <158847938+velozerick@users.noreply.github.com>
2025-05-25 20:28:00 -0600

    Update Data_Type.java

--- 1_CH/Tipos_De_Datos/Data_Type.java
+++ 1_CH/Tipos_De_Datos/Data_Type.java
@@ -1,1 +1,11 @@
+/*
+Los datos son representaciones del mundo real (edad, numero, flota de camiones , etc.)
+JAVA es un lenguaje fuertemente tipado y define 8 tipos de datos primitivos:
 
+
+Integers/Enteros = byte, short, int, long, 
+FLoating-point numbers, double/flotantes = representa numeros con precision fraccional 
+Characters/caracteres = char, representa simbolos como letras y numeros (datos alfanumericos)
+Boolean/datos logicos = representa verdadero o falso 
+
+*/
