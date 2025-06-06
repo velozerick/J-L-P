7a82fe7cae18e90741f1d05091006a12b02e5500
Erick Veloz <158847938+velozerick@users.noreply.github.com>
2025-06-06 10:07:26 -0600

    Update and rename Data_Type.java to DataType.java

---
+++ 1_CH/Tipos_De_Datos/DataType.java
@@ -0,0 +1,89 @@
+/*
+Los datos son representaciones del mundo real (edad, numero, flota de camiones , etc.)
+JAVA es un lenguaje fuertemente tipado y define 8 tipos de datos primitivos:
+
+
+Integers/Enteros = byte, short, int, long, 
+FLoating-point numbers, double/flotantes = representa numeros con precision fraccional 
+Characters/caracteres = char, representa simbolos como letras y numeros (datos alfanumericos)
+Boolean/datos logicos = representa verdadero o falso 
+
+Pero antes de eso recordemos algo 
+
+OPERACIONES ARITMETICAS
+suma +
+resta -
+multiplicacion *
+division /
+modulo (resto) %
+
+operador incremento ++: se utiliza para aumentar en una unidad el valor de una variable numerica entera
+
+operador decremento - : se utiliza para disminuir en una unidad el valor de una variable numerica entera
+
+OPERACIONES LOGICAS
+
+ AND &&
+ OR ||
+ not !
+
+
+menor que <
+mayor que >
+igual que ==
+menor o igual que <=
+mayor o igual que >=
+distinto de !=
+
+
+
+
+#### Ver variables y regresar
+*/
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Tipo de datos primitivos
+		
+		//Datos enteros
+		int myInt = 22;
+		System.out.println(myInt);
+		
+		
+		//Double datos decimales 
+		double myDouble = 22.16486;
+		System.out.println(myDouble);
+		
+		
+		//char un solo caracter
+		char myChar = 'e';
+		System.out.println(myChar);
+		
+		
+		// float es como double pero con formacion difrerente
+		float myFloat =  23;
+		System.out.println(myFloat);
+		
+		
+		//boolean el verdaero o falso 
+		boolean myBoolean = true;
+		System.out.println(myBoolean);
+		
+		/*      Son otros tipos de datos para trabajar con nuemros
+		 float
+		 long
+		 byte
+		 */
+	
+	}
+
+}
+
+
+
+
+
