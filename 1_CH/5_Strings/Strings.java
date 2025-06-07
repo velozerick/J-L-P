e40bb453afd57f6c9e41c05372a903699c9cb290
Erick Veloz <158847938+velozerick@users.noreply.github.com>
2025-06-07 14:57:18 -0600

    Update Strings.java

--- 1_CH/5_Strings/Strings.java
+++ 1_CH/5_Strings/Strings.java
@@ -94,12 +94,62 @@ public class Strings {
 
         System.out.println(String.format("Hola mi nombre es %s tengo %d años." , name , age )); // %s es para cadenas de texto y %d para numeros enteros de esta manera se reemplazan los valores en la cadena de texto
         
-        
-        
-        
-
-
-
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+package ch_1.Strings;
+
+public class StringsExercises {
+    public static void main(String[] args){
+
+        // 1. Concatena dos cadenas de texto.
+        String name = "Salvador";
+        String surname = "Skywalker";
+
+        System.out.println(name + " " + surname);
+
+        // 2. Muestra la longitud de una cadena de texto.
+        System.out.println(name.length());
+
+        // 3. Muestra el primer y último carácter de un string.
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(name.length() - 1));
+
+        // 4. Convierte a mayúsculas y minúsculas un string.
+        System.out.println(name.toUpperCase());
+        System.out.println(surname.toLowerCase());
+
+        // 5. Comprueba si una cadena de texto contiene una palabra concreta.
+        System.out.println(name.contains("Sky"));
+        System.out.println(surname.contains("wal"));
+
+        // 6. Formatea un string con un entero.
+        int age = 22;
+        System.out.println(String.format("Mi nombre es %s y tengo %d años de edad.", name , age));
+
+        // 7. Elimina los espacios en blanco al principio y final de un string.
+        System.out.println(String.format("hola me llamo %s y tengo %d años de edad.",name , age).trim());
+
+        // 8. Sustituye todos los espacios en blanco de un string por un guión (-).
+        System.out.println(String.format("hola me llamo %s y tengo %d años de edad.",name , age).replace(" ", "-"));
+
+        // 9. Comprueba si dos strings son iguales.
+        System.out.println(name.equals(surname));
+
+        // 10. Comprueba si dos strings tienen la misma longitud.
+        System.out.println(name.length() == surname.length());
+    }
+
+}
+
