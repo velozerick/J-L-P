ea29144a63ba40b89033ee12c34e33947768b252
Erick Veloz <158847938+velozerick@users.noreply.github.com>
2025-06-06 20:02:05 -0600

    Update operators.java

--- 1_CH/4_Operators/operators.java
+++ 1_CH/4_Operators/operators.java
@@ -1,1 +1,25 @@
 
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Operadores nos permiten relaizar operaciones sobre valores 
+		
+		//Operadores aritmeticos
+		var a = 15;
+		var b = 10;
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		
+		
+	
+	
+	
+	}
+
+}
+
