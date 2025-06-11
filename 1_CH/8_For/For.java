4c07c458ef54fc475f7821402e82643587c645ad
Erick Veloz <158847938+velozerick@users.noreply.github.com>
2025-06-11 14:47:37 -0600

    Update For.java

--- 1_CH/8_For/For.java
+++ 1_CH/8_For/For.java
@@ -1,1 +1,34 @@
+package ch_1.For;
+
+import java.util.Scanner;
+
+public class For {
+    public static void main(String[] args) {
+        //The complete reference pag 101
+
+        //Cuenta regresiva
+
+        int n; //declaramos n
+
+        for (n = 10; n > 0; n--)
+            System.out.println("tick" + n);
+
+
+
+        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Porfavor ingresa un numero para iniciar la cuenta regresiva:  ");
+        int numero = entrada.nextInt();
+        
+        for (; numero > 0; numero--)
+            System.out.println("tick" + numero);
+
+
+        entrada.close();
+        
+         
+
+    }
+}
 
